d7a23cbd1098cceb671de80538c6ebc6139df535
nopain8269 <146239597+nopain8269@users.noreply.github.com>
2023-10-13 13:41:46 +0100

    Add files via upload
    additional language pack

---
+++ locales/de.lua
@@ -0,0 +1,14 @@
+Locales['de'] = {
+    ['sellweed'] = 'Verkaufe Weed',
+    ['sellmeth'] = 'Verkaufe Meth',
+    ['sellcoke'] = 'Verkaufe Kokain',
+    ['negotiateprice'] = 'Preis verhandeln',
+    ['cancel'] = 'Der Käufer hat dein Angebot abgelehnt',
+    ['notifypolice'] = 'Drogenverkauf',
+    ['toofar'] = 'Du bist zu weit gegangen',
+    ['sameguy'] = 'Du hast bereits mit der Person gesprochen',
+    ['nopolice'] = "Nicht genug Polizisten auf Streife",
+    ['yousold'] = "Du verkaufst ",
+    ['for'] = "für ",
+    ['currency'] = "$",
+}
