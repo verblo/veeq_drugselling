f72f02e1e8ca6e3b9ada9abd33bf431d9f52b816
VeeQ <46843019+verblo@users.noreply.github.com>
2023-11-20 18:50:37 +0100

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,13 @@
+Locales['fr'] = {
+    ['sellweed'] = "Vendre de la weed",
+    ['sellmeth'] = "Vendre de la meth",
+    ['sellcoke'] = 'Vendre de la coke',
+    ['cancel'] = "Le piéton a décliné votre offre",
+    ['notifypolice'] = 'Vente de drogue',
+    ['toofar'] = 'Vous êtes allé trop loin',
+    ['sameguy'] = "Vous avez déjà parlé à cette personne",
+    ['nopolice'] = "Pas assez de policiers en service",
+    ['yousold'] = "Vous avez vendu ",
+    ['for'] = " pour ",
+    ['currency'] = " $",
+}
